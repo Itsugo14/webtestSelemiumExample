1f86c240dd80af00a2fe2d9953450bcc430b16d3
Anders Børjesson <anbo@zealand.dk>
2019-06-25 09:59:59 +0200

    Add project files.

---
+++ Selenium1Example/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Selenium1Example
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
